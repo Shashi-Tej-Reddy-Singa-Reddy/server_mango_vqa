c9327968b93b62270a7682800d7f01729f4a386d
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2024-12-17 16:01:17 +0530

    k

--- server.js
+++ server.js
@@ -26,8 +26,6 @@ app.use(cookieParser());
 // Connect to MongoDB
 mongoose
   .connect(process.env.REACT_APP_MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB:', err));
